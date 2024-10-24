1e409d33e8d952f0291a3e047e2bde8c86d8aa56
Khanomix <123134542+Khanomix@users.noreply.github.com>
2024-10-24 23:50:30 +0330

    kji

--- colab_leecher/utility/variables.py
+++ colab_leecher/utility/variables.py
@@ -69,20 +69,20 @@ class BotTimes:
 
 
 class Paths:
-    WORK_PATH = "/app/BOT_WORK"
-    THMB_PATH = "/app/colab_leecher/Thumbnail.jpg"
+    WORK_PATH = "/BOT_WORK"
+    THMB_PATH = "/colab_leecher/Thumbnail.jpg"
     VIDEO_FRAME = f"{WORK_PATH}/video_frame.jpg"
     HERO_IMAGE = f"{WORK_PATH}/Hero.jpg"
-    DEFAULT_HERO =  "/app/custom_thmb.jpg"
-    MOUNTED_DRIVE = "/app/drive"
+    DEFAULT_HERO =  "/custom_thmb.jpg"
+    MOUNTED_DRIVE = "/drive"
     down_path = f"{WORK_PATH}/Downloads"
     temp_dirleech_path = f"{WORK_PATH}/dir_leech_temp"
-    mirror_dir = "/app/drive/MyDrive/Colab Leecher Uploads"
+    mirror_dir = "/drive/MyDrive/Colab Leecher Uploads"
     temp_zpath = f"{WORK_PATH}/Leeched_Files"
     temp_unzip_path = f"{WORK_PATH}/Unzipped_Files"
     temp_files_dir = f"{WORK_PATH}/leech_temp"
     thumbnail_ytdl = f"{WORK_PATH}/ytdl_thumbnails"
-    access_token = "/app/token.pickle"
+    access_token = "/token.pickle"
 
 
 class Messages:
